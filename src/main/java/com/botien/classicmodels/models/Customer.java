7cd17283a1e4d4dae8b7e7442a62d5510b0bdbb6
bonfotieno <bonnyotieno9@gmail.com>
2023-01-07 13:09:50 +0300

    added customer model

---
+++ src/main/java/com/botien/classicmodels/models/Customer.java
@@ -0,0 +1,176 @@
+package com.botien.classicmodels.models;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "customers")
+public class Customer implements Serializable {
+
+    @Id
+    @Column(name = "customer_number", nullable = false)
+    private Integer customerNumber;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "customer_name", nullable = false, length = 50)
+    private String customerName;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "contact_last_name", nullable = false, length = 50)
+    private String contactLastName;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "contact_first_name", nullable = false, length = 50)
+    private String contactFirstName;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "phone", nullable = false, length = 50)
+    private String phone;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "address_line1", nullable = false, length = 50)
+    private String addressLine1;
+
+    @Size(max = 50)
+    @Column(name = "address_line2", length = 50)
+    private String addressLine2;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "city", nullable = false, length = 50)
+    private String city;
+
+    @Size(max = 50)
+    @Column(name = "state", length = 50)
+    private String state;
+
+    @Size(max = 15)
+    @Column(name = "postal_code", length = 15)
+    private String postalCode;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "country", nullable = false, length = 50)
+    private String country;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sales_rep_employee_number",foreignKey = @ForeignKey(name = "customers_ibfk_1"))
+    private Employee salesRepEmployeeNumber;
+
+    @Column(name = "credit_limit", precision = 10, scale = 2)
+    private BigDecimal creditLimit;
+
+
+    public Integer getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(Integer customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getContactLastName() {
+        return contactLastName;
+    }
+
+    public void setContactLastName(String contactLastName) {
+        this.contactLastName = contactLastName;
+    }
+
+    public String getContactFirstName() {
+        return contactFirstName;
+    }
+
+    public void setContactFirstName(String contactFirstName) {
+        this.contactFirstName = contactFirstName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Employee getSalesRepEmployeeNumber() {
+        return salesRepEmployeeNumber;
+    }
+
+    public void setSalesRepEmployeeNumber(Employee salesRepEmployeeNumber) {
+        this.salesRepEmployeeNumber = salesRepEmployeeNumber;
+    }
+
+    public BigDecimal getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(BigDecimal creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+}
